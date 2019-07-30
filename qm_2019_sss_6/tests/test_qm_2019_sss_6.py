a1ed433c3f97890a7da0ebb6f4d9a388d3baf24f
Gabriel Phun <gphun@uci.edu>
2019-07-30 16:55:00 -0500

    added more tests

--- qm_2019_sss_6/tests/test_qm_2019_sss_6.py
+++ qm_2019_sss_6/tests/test_qm_2019_sss_6.py
@@ -36,6 +36,8 @@ vec = {'px': [1, 0, 0], 'py': [0, 1, 0], 'pz': [0, 0, 1]}
 orbital_occupation = { 's':0, 'px':1, 'py':1, 'pz':1 }
 
 my_model = qm_2019_sss_6.NobleGasModel(atomic_coordinates,model_parameters,ionic_charge,orbital_types,orbitals_per_atom,vec,orbital_occupation)
+interaction_matrix,chi_tensor,hamiltonian_matrix,density_matrix,energy_ion = my_model.kernel()
+my_scf = qm_2019_sss_6.scf(hamiltonian_matrix,interaction_matrix,density_matrix,chi_tensor,energy_ion,ionic_charge,orbitals_per_atom)
 
 def test_qm_2019_sss_6_imported():
     """Sample test, will always pass so long as import statement worked"""
@@ -169,16 +171,76 @@ def test_calculate_interaction_matrix():
     assert np.allclose( my_model.calculate_interaction_matrix(atomic_coordinates, model_parameters), expected_matrix, rtol=1e-01)
 
 
-# def test_calculate_energy_ion():
+def test_calculate_energy_ion():
+    expected_number = 5.091168824543142
+
+    assert my_model.calculate_energy_ion(atomic_coordinates) == expected_number
+
+def test_coulomb_energy():
+    coulomb_vector = np.array([-1,0,0])
+    assert my_model.coulomb_energy('s','s',coulomb_vector) == 1.0
+    assert my_model.coulomb_energy('s','px',coulomb_vector) == -1.0
+    assert my_model.coulomb_energy('px','px',coulomb_vector) == -2.0
+    assert my_model.coulomb_energy('s','py',coulomb_vector) == 0.0
+    assert my_model.coulomb_energy('py','px',coulomb_vector) == 0.0
+    assert my_model.coulomb_energy('py','py',coulomb_vector) == 1.0
+
+
+def test_calculate_potential_vector():
+    expected_vector = np.array([-0.8, -0.1, -0.1, -0.1, -0.8,  0.1,  0.1,  0.1])
+    assert np.allclose(my_model.calculate_potential_vector(atomic_coordinates, model_parameters), expected_vector, rtol=1e-0)
+
+
+def test_chi_on_atom():
+    expected_number = 1.0
+    assert my_model.chi_on_atom('s', 's', 's', model_parameters) == expected_number
+
+def test_pseudopotential_energy():
+    pseudo_vector = np.array([-model_parameters['r_pseudo'],0,0])
+    expected_number = 0.04594598437272995
+    assert my_model.pseudopotential_energy('px',pseudo_vector, model_parameters) == expected_number
+
+def test_hopping_energy():
+    hop_vector = np.array([-model_parameters['r_hop'],0,0])
+    expected_number = 0.03365982238611262
+    assert my_model.hopping_energy('s','s',hop_vector,model_parameters) == expected_number
+
+
+def test_kernel():
+    interaction_matrix = my_model.calculate_interaction_matrix(my_model.atomic_coordinates, my_model.model_parameters)
+    chi_tensor = my_model.calculate_chi_tensor(my_model.atomic_coordinates, my_model.model_parameters)
+    hamiltonian_matrix = my_model.calculate_hamiltonian_matrix(my_model.atomic_coordinates, my_model.model_parameters)
+    density_matrix = my_model.calculate_atomic_density_matrix(my_model.atomic_coordinates)
+    energy_ion = my_model.calculate_energy_ion(my_model.atomic_coordinates)
+
+    assert np.allclose( interaction_matrix, my_model.kernel()[0], rtol=1e-0)
+    assert np.allclose( chi_tensor, my_model.kernel()[1], rtol=1e-0)
+    assert np.allclose( hamiltonian_matrix, my_model.kernel()[2], rtol=1e-0)
+    assert np.allclose( density_matrix, my_model.kernel()[3], rtol=1e-0)
+    assert np.allclose( energy_ion, my_model.kernel()[4], rtol=1e-0)
+
+# def test_scf_cycle():
+#     fock_matrix = my_scf.calculate_fock_matrix(hamiltonian_matrix, interaction_matrix, density_matrix, chi_tensor)
+#     density_matrix = my_scf.calculate_density_matrix(fock_matrix)
+#     expected_matrix = np.array([[ 5.4e+00,  2.6e-04,  3.5e-04,  4.4e-04,  6.3e-04,  5.5e-04,  7.3e-04,  9.2e-04],
+#     [ 2.6e-04, -5.9e-01,  4.7e-09,  5.8e-09, -5.5e-04,  3.0e-04,  2.2e-03,  2.7e-03],
+#     [ 3.5e-04,  4.7e-09, -5.9e-01,  7.8e-09, -7.3e-04,  2.2e-03,  1.6e-03,  3.6e-03],
+#     [ 4.4e-04,  5.8e-09,  7.8e-09, -5.9e-01, -9.2e-04,  2.7e-03,  3.6e-03,  3.2e-03],
+#     [ 6.3e-04, -5.5e-04, -7.3e-04, -9.2e-04,  5.4e+00, -2.6e-04, -3.5e-04, -4.4e-04],
+#     [ 5.5e-04,  3.0e-04,  2.2e-03,  2.7e-03, -2.6e-04, -5.9e-01,  4.7e-09,  5.8e-09],
+#     [ 7.3e-04,  2.2e-03,  1.6e-03,  3.6e-03, -3.5e-04,  4.7e-09, -5.9e-01,  7.8e-09],
+#     [ 9.2e-04,  2.7e-03,  3.6e-03,  3.2e-03, -4.4e-04,  5.8e-09,  7.8e-09, -5.9e-01]])
 #
-# def test_coulomb_energy():
+#     density_matrix, fock_matrix = my_scf.scf_cycle(hamiltonian_matrix, interaction_matrix, density_matrix, chi_tensor)
+#     assert np.allclose(expected_matrix, fock_matrix, rtol=1e-0)
+
+
+# def test_calculate_energy_scf():
 #
-# def test_calculate_potential_vector():
+# def test_calculate_density_matrix():
 #
-# def test_chi_on_atom():
+# def test_fast_fock_matrix():
 #
-# def test_pseudopotential_energy():
+# def test_calculate_fock_matrix():
 #
-# def test_hopping_energy():
-#
-# def test_kernel():
+# def test_initialize():
